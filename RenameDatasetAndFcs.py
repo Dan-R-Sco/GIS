c535672bb7cf9258812f662ec887a81ae71f3ec6
polecatleader <dr_scott8@hotmail.com>
2017-11-21 13:15:31 -0300

    added comments

--- RenameDatasetAndFcs.py
+++ RenameDatasetAndFcs.py
@@ -10,6 +10,6 @@ for fc in fclist:
     print fc
     fcstripped = fc.lstrip('CL_DVC.OWD.') #strip the prefixe
     print fcstripped
-    name = "CL_DVC.OWD." + newname + "_" + fcstripped #
+    name = "CL_DVC.OWD." + newname + "_" + fcstripped #change directory prefix according to db name and dataset name
     print name
     arcpy.Rename_management(fc, name)
