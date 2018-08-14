9e92ab73cecf8695f5c4310c9f51c14f25fcec95
polecatleader <dr_scott8@hotmail.com>
2018-08-14 18:33:11 -0300

    Update RenameFC.py

--- RenameFC.py
+++ RenameFC.py
@@ -1,10 +1,10 @@
 #script to rename fcs in a template dataset to remove the _1's
 
 import arcpy
-
-arcpy.env.workspace = r'Database Connections\Dataowner@SRV-SQLHA02@DVC.sde'
-
-fclist = arcpy.ListFeatureClasses("","",'W1709')
+#input the db connection below i.e. r'Database Connections\User@server.sde
+arcpy.env.workspace = <INPUT DB CONNECTION>
+#List fc's that contain a key word
+fclist = arcpy.ListFeatureClasses("","",<INPUT KEYWORD>)
 for fc in fclist:
     if fc.endswith("_1"):
         newname = fc.replace('_1',"")
