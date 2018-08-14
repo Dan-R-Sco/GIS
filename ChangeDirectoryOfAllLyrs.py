5aa923fa6f35586d8d45023b785f2bb4dcb53a28
polecatleader <dr_scott8@hotmail.com>
2018-08-14 10:24:46 -0300

    Update and rename ChangeDirectory.py to ChangeDirectoryOfAllLyrs.py

--- ChangeDirectoryOfAllLyrs.py
+++ ChangeDirectoryOfAllLyrs.py
@@ -1,3 +1,7 @@
+""" 
+Script that cycles through a current mxd and searches the source of each layer and replaces it with something else 
+"""
+
 import arcpy
 mapdoc = arcpy.mapping.MapDocument("CURRENT")
 mapdoc.findAndReplaceWorkspacePaths("<the directory you are changing from>" , "<the directory you are changing to>")
