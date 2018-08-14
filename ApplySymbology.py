de3cf55b1c48a2bc4320f4c2db63281aa61daf99
polecatleader <dr_scott8@hotmail.com>
2018-08-14 10:15:54 -0300

    Update ApplySymbology.py

--- ApplySymbology.py
+++ ApplySymbology.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed May 02 12:11:03 2018
-apply symbology
+apply symbology from a saved layer has to be used inside the mxd that you want to change the layer
 @author: daniel.scott
 """
 # the MXDs to change, accessed and processed individually as 'i' is script not shown
 import arcpy
+## input the symbol lyr to apply i.e. "C:\Docs\Hollow.lyr"
+sym_to_apply = 
 mxd = arcpy.mapping.MapDocument(r'CURRENT')
+#cycle through lyrs in mxd and apply the symbology
 for lyr in arcpy.mapping.ListLayers(mxd):
-        arcpy.ApplySymbologyFromLayer_management(lyr,r"Q:\08_EXINT\01_KM\02_Projects\Search tool\Hollow.lyr")
-
-mxd.saveACopy(r"Q:\08_EXINT\01_KM\02_Projects\Search tool\VectorIndex.mxd")
+        arcpy.ApplySymbologyFromLayer_management(lyr, sym_to_apply)
+#save the mxd
+mxdsave_dest = r"C:\Docs\VectorIndex.mxd"
+mxd.saveACopy(mxdsave_dest)
