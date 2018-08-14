448be0504eee1b8bb479811f817842a2f0bef2dc
polecatleader <dr_scott8@hotmail.com>
2018-08-14 11:08:45 -0300

    Delete CopyTemplateRename.py

--- CopyTemplateRename.py
+++
@@ -1,9 +0,0 @@
-#renaming dataset and then FCs in edit still
-
-import arcpy
-
-arcpy.env.workspace = r'Database Connections\Dataowner@SRV-SQLHA02@DVC.sde'
-
-outWorkspace = r'Database Connections\Dataowner@SRV-SQLHA02@DVC.sde'
-print "Copying Template and writing Test "
-arcpy.CopyFeatures_management("Template","Test")
