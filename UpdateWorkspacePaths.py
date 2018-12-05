16409e9b7e40ba9873a88f210de8733d7f9804cf
Dan-R-Sco <dr_scott8@hotmail.com>
2018-12-05 11:38:14 -0300

    Update and rename ChangeDirectoryOfAllLyrs.py to UpdateWorkspacePaths.py

--- UpdateWorkspacePaths.py
+++ UpdateWorkspacePaths.py
@@ -1,5 +1,9 @@
 """ 
 Script that cycles through a current mxd and searches the source of each layer and replaces it with something else 
+
+Inputs:
+-Current MXD
+-Find and Replace workspace paths
 """
 
 import arcpy
